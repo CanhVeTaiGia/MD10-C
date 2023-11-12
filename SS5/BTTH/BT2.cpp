2d0e7c89d3ac3f5449538fab187f8568bd6d5a92
CanhVeTaiGia <147359294+CanhVeTaiGia@users.noreply.github.com>
2023-11-12 16:48:42 +0700

    Add files via upload

--- SS5/BTTH/BT2.cpp
+++ SS5/BTTH/BT2.cpp
@@ -6,9 +6,7 @@ int main(){
 	if(aCom<=300){
 		if(aCom<=100){
 			printf("Hoa hong dai ly cua cua hang la %d",sales =aCom*(5/100));
-		} if(aCom<=50){
-			printf("Hoa hong dai ly cua cua hang la %d",sales =aCom*(10/100));
-			}
+		}
 	} else
 	printf("Hoa hong dai ly cua cua hang la %d",sales=aCom*(20/100));
 }
