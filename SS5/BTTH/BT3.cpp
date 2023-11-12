d306d7b13cdc28be42c068b97c2c306bfe13b0e8
CanhVeTaiGia <147359294+CanhVeTaiGia@users.noreply.github.com>
2023-11-12 17:15:05 +0700

    Add files via upload

--- SS5/BTTH/BT3.cpp
+++ SS5/BTTH/BT3.cpp
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main(){
-	int minutes,sF = 25000;
+	int minutes,sF = 25000,rates;
 	printf("Nhap vao so phut su dung\n");
 	scanf("%d",&minutes);
 		if(minutes<=200){
 			if(minutes<=150){
 				if(minutes<=50){
-					printf("Gia cuoc cua ban la %d",minutes*600+sF);
+					printf("Gia cuoc cua ban la %d",rates=minutes*600+sF);
 				} else
-				printf("Gia cuoc cua ban la %d",(minutes-50)*400+sF);
+				printf("Gia cuoc cua ban la %d",rates=(minutes-50)*400+sF);
 			} else
-			printf("Gia cuoc cua ban la %d",(minutes-150)*200+(150*600)+(50*600)+sF);
+			printf("Gia cuoc cua ban la %d",rates=(minutes-150)*200+(150*600)+(50*600)+sF);
 		} else 
-		printf("Gia cuoc cua ban la %d",(minutes-200)*100+(50*400)+(100*600)+(50*600)+sF);
+		printf("Gia cuoc cua ban la %d",rates=(minutes-200)*100+(50*400)+(100*600)+(50*600)+sF);
 	}
